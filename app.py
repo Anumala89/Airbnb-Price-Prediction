9b66f9c7581a9c7dbb5782387131cd504935f45b
jdkwak1994 <jdkwak1994@gmail.com>
2021-02-01 21:20:48 -0500

    adding the group project file

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template, redirect, request, jsonify
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
-app.static_folder = 'templates/static'
 
-@app.route("/")
+app = Flask(__name__)
+app.static_folder = "templates/static"
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        testdf = []
+        boroughtype = int(request.form["boroughradio"])
+        roomtype = int(request.form["roomtyperadio"])
+        review_input = request.form["reviewmonth"]
+        avail_input = request.form["availability"]
+        for x in range(5):
+            if boroughtype == 1:
+                testdf.append(1)
+            else:
+                testdf.append(0)
+            boroughtype = boroughtype - 1
+        for x in range(3):
+            if roomtype == 1:
+                testdf.append(1)
+            else:
+                testdf.append(0)
+            roomtype = roomtype - 1
+        testdf.append(float(review_input))
+        testdf.append(int(avail_input))
+        print(testdf)
+        return render_template("index.html", InputData=testdf)
+    else:
+        return render_template("index.html")
 
 
 @app.route("/infographic")
